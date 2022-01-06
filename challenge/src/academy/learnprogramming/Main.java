0703a279b5cb5aeafa6577610442792fbdab3740
JeremyJMoss <mossyj19@gmail.com>
2022-01-06 23:40:51 +1100

    more exercise

--- challenge/src/academy/learnprogramming/Main.java
+++ challenge/src/academy/learnprogramming/Main.java
@@ -3,34 +3,20 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	    printDayOfTheWeek(3);
+	    int count = 0;
+        int sum = 0;
+        for (int i = 1; i <=1000; i++){
+            if (i % 3 == 0 && i % 5 == 0) {
+                count++;
+                System.out.println(i + " is divisible by 3 and 5");
+                sum += i;
+                if (count == 5) {
+                    break;
+                }
+            }
+        }
+        System.out.println("The sum of the numbers was " + sum);
     }
 
-    public static void printDayOfTheWeek(int day){
-        switch(day){
-            case 0:
-                System.out.println("Sunday");
-                break;
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            default:
-                System.out.println("Invalid day");
-        }
-    }
+
 }
